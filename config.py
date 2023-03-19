fc1e44655801ea75499c1ded30c50c650f9ddfaf
Dusikak <117767242+Dusikak@users.noreply.github.com>
2023-03-19 15:45:56 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = '5727842010:AAH9CZO3cTBRa5ATkTCNNaK6r_VLkxvounw'
-
-access_key = 'fEOlPZGgsrfXZSnVueWZVtNlpXfyKqOn'
-
-exchenges = {
-    'доллар': 'USD',
-    'евро': 'EUR',
-    'рубль': 'RUB'
-}
